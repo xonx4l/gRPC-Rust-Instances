65488eb1f3f34f3eb0c7be9fab3c5fdfbdf7b8ad
xonx4l <sk797119@gmail.com>
2025-08-14 23:55:25 +0530

    update chat_server.rs

--- src/chat_server.rs
+++ src/chat_server.rs
@@ -7,3 +7,16 @@ pub mod chatter {
 #[derive(Debug, Default)]
 pub struct MyChatter {}
 
+#[tonic::async_trait]
+impl Chatter for MyChatter {
+    type ChatStreamStream = 
+        tokio_stream::wrappers::ReceiverStream<Result<ChatMessage, Status>>;
+
+        async_fn_chat_stream(
+            &self,
+            request: Request<Streaming<ChatMessage>>,
+        )-> Result<Response<Self::ChatStreamStream>,Status> {
+             let mut in_Stream = request_into_inner();
+             let (tx,rx) = mpsc::channel(128);
+        }
+}
