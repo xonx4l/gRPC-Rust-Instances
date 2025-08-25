c44b25a7ee19ecc8e8a477bb6e165172da1e0bac
xonx4l <sk797119@gmail.com>
2025-08-25 22:31:48 +0530

    update chat_client.rs

--- src/chat_client.rs
+++ src/chat_client.rs
@@ -15,4 +15,26 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
 tokio::spawn(async move {
     let mut stdin = io::BufReader::new(io::stdin()).lines();
     println!("Enter your messages. Type 'exit' to quit.");
-})
+    loop {
+        match stdin.next_line().await {
+            Ok(Some(line)) => {
+                if line.trim().is_empty(){
+                    continue;
+                }
+                if line.eq_ignore_ascii_case("exit"){
+                     println!("Exiting.");
+                     break;
+                }
+                let msg = ChatMessage {
+                    user_id: "RustClient".to_string(),
+                    text: line,
+                };
+                if tx.send(msg).await.is_err(){
+                    eprintln!("Failed to send message . Server Closed Connection.");
+                    break;
+                }
+            }
+            _ => break,
+        }
+    }
+});
